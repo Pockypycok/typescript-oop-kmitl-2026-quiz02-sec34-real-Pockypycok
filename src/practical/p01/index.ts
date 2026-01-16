f9f3d7ec895c106af3f2be639bb4621b784fb86c
pockypycok <cheophy8@gmail.com>
2026-01-16 20:01:21 +0700

    drr

--- src/practical/p01/index.ts
+++ src/practical/p01/index.ts
@@ -1,1 +1,55 @@
-export function getPostalAddress() {}
+import axios from "axios";
+
+interface Geo {
+  lat: string;
+  lng: string;
+}
+
+interface Address {
+  street: string;
+  suite: string;
+  city: string;
+  zipcode: string;
+  geo: Geo;
+}
+
+interface User {
+  id: number;
+  name: string;
+  phone: string;
+  address: Address | null;
+}
+
+export async function getPostalAddress(): Promise<User[]> {
+  try {
+    const url = "https://jsonplaceholder.typicode.com/users";
+    const response = await axios.get("url");
+    const users = response.data;
+
+    if (!users || users.length === 0) {
+      return [];
+    }
+
+    return users.map((user: any) => ({
+      id: user.id,
+      name: user.name,
+      phone: user.phone,
+      address: user.address
+        ? {
+            street: user.address.street,
+            suite: user.address.suite,
+            city: user.address.city,
+            zipcode: user.address.zipcode,
+            geo: user.address.geo
+              ? {
+                  lat: user.address.geo.lat,
+                  lng: user.address.geo.lng,
+                }
+              : { lat: "", lng: "" },
+           }
+        : null,
+    }));
+  } catch (error) {
+    return [];
+  }
+}
